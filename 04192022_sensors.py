5f92dd32f77fe450e7f446374f4feff87d333a0d
Kevin Zhu <33162506+zhuk48@users.noreply.github.com>
2022-04-20 19:02:29 -0500

    played around with data a bit

--- 04192022_sensors.py
+++ 04192022_sensors.py
@@ -1,15 +1,15 @@
 from microbit import *
 import math
 
-n = 2 # number of bytes read from SCL and SDA pins
+n = 64 # number of bytes read from SCL and SDA pins
+i2c.init()
+devices = i2c.scan() # Returns list of addresses corresponding to devices that responded to scan
+print(devices)
 
 while True:
-
-    i2c.init()
-    devices = i2c.scan() # Returns list of addresses corresponding to devices that responded to scan
-    print(devices)
-    
-    # Uncomment these lines once scan() returns something (i.e. devices is not an empty list)
-    #imu1_reading = 2c.read(devices[0], n, repeat=False)
+    imu1_reading = i2c.read(devices[0], n, repeat=False)
     #imu2_reading = i2c.read(devices[1], n, repeat=False)
-    #print(imu1_reading, imu2_reading)
+    imu1_accel = imu1_reading[59:64]
+    #output = int.from_bytes(imu1_accel, "little")
+    print(imu1_accel)
+    sleep(1000)
