9082d341429cf823be062f0a41ff99e76ac1c371
Kevin Zhu <33162506+zhuk48@users.noreply.github.com>
2022-04-21 19:29:22 -0500

    can read accel and gyro values

--- 04192022_sensors.py
+++ 04192022_sensors.py
@@ -1,15 +1,52 @@
+# Add your Python code here. E.g.
 from microbit import *
-import math
 
-n = 64 # number of bytes read from SCL and SDA pins
-i2c.init()
-devices = i2c.scan() # Returns list of addresses corresponding to devices that responded to scan
+display.scroll("Posture Pal", wait=False, loop=True)
+
+# INIT I2C
+i2c.init(freq=100000, sda=pin20, scl=pin19)
+devices = i2c.scan()
 print(devices)
 
+# CHECK WAI ON MPU
+i2c.write(104, b'\x75')
+wai_reading = i2c.read(104, 1, repeat=False)
+WAI = int.from_bytes(wai_reading, "little")
+print(WAI) # should return 0x71 or 113
+
+
 while True:
-    imu1_reading = i2c.read(devices[0], n, repeat=False)
-    #imu2_reading = i2c.read(devices[1], n, repeat=False)
-    imu1_accel = imu1_reading[59:64]
-    #output = int.from_bytes(imu1_accel, "little")
-    print(imu1_accel)
+    i2c.write(104, b'\x3b', repeat=False)
+    ACCEL0_X_H = i2c.read(104, 1, repeat=False)
+    sleep(1)
+    i2c.write(104, b'\x3c', repeat=False)
+    ACCEL0_X_L = i2c.read(104, 1, repeat=False)
+    ACCEL0_X = ACCEL0_X_H + ACCEL0_X_L
+    print(ACCEL0_X)
+    
+    #i2c.write(104, b'\x3d')
+    #ACCEL0_Y_H = i2c.read(104, 1, repeat=False)
+    #sleep(1)
+    #i2c.write(104, b'\x3e')
+    #ACCEL0_Y_L = i2c.read(104, 1, repeat=False)
+    #ACCEL0_Y = ACCEL0_Y_H + ACCEL0_Y_L
+    #print(ACCEL0_Y)
+    
+    #i2c.write(104, b'\x47')
+    #ACCEL0_Z_H = i2c.read(104, 1, repeat=False)
+    #i2c.write(104, b'\x48')
+    #ACCEL0_Z_L = i2c.read(104, 1, repeat=False)
+    #ACCEL0_Z = ACCEL0_Z_H + ACCEL0_Z_L
+    #print(ACCEL0_Z)
+    
+    i2c.write(104, b'\x43', repeat=False)
+    GYRO0_X_H = i2c.read(104, 1, repeat=False)
+    sleep(1)
+    i2c.write(104, b'\x44', repeat=False)
+    GYRO0_X_L = i2c.read(104, 1, repeat=False)
+    GYRO0_X = GYRO0_X_H + GYRO0_X_L
+    print(GYRO0_X)
+    
+    print('\n')
+
     sleep(1000)
