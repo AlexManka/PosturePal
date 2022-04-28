1bb175cd9f0a08a828075f449bd8a240c52c6774
Alex Manka <alexmanka2022@u.northwestern.edu>
2022-04-28 14:12:01 -0500

    Add files via upload
    Able to read Acc Z-axis but not others on MPU 9250

---
+++ Posture_Pal.py
@@ -0,0 +1,68 @@
+# Add your Python code here. E.g.
+from microbit import *
+
+i2c.init(freq=100000, sda=pin20, scl=pin19)
+# print(int(i2c.scan()[0]))
+# 104 was printed
+i2caddr = 104
+
+# takes in a reg (int) and data (hex/bin)
+def write_reg(reg_addr, data):
+    i2c.write(i2caddr, reg_addr.to_bytes(1, 'big') + data)
+
+# takes in a reg (int) and number of bytes to read n (int)
+def read_reg(reg_addr, n):
+    i2c.write(i2caddr, reg_addr.to_bytes(1, 'big'))
+    return i2c.read(i2caddr, n)
+
+# acceleration conversion factor (from datasheet)
+aconv = 16384.0
+
+
+# write_reg(119, b'\x00')
+# write_reg(120, b'\x00')
+# write_reg(122, b'\x00')
+# write_reg(123, b'\x00')
+# write_reg(125, b'\x00')
+# write_reg(126, b'\x00')
+
+
+
+while True:
+    #AccX
+    axhi = read_reg(59, 1)
+    axlo = read_reg(60, 1)
+    ax = axhi + axlo
+    print(int.from_bytes(ax, 'big') / aconv)
+    print('\n')
+    
+    #AccY
+    ayhi = read_reg(61, 1)
+    aylo = read_reg(62, 1)
+    ay = ayhi + aylo
+    print(int.from_bytes(ax, 'big') / aconv)
+    print('\n')
+    
+    #AccZ
+    azhi = read_reg(63, 1)
+    azlo = read_reg(64, 1)
+    az = azhi + azlo
+    print(int.from_bytes(az, 'big') / aconv)
+    print('\n')
+    
+    print(int.from_bytes(read_reg(117, 1), 'big'))
+    
+    
+    # Print out the micro:bit values
+    print(accelerometer.get_values())
+    print('\n\n')
+    
+    # print(read_reg(119, 1))
+    # print(read_reg(120, 1))
+    # print(read_reg(122, 1))
+    # print(read_reg(123, 1))
+    # print(read_reg(125, 1))
+    # print(read_reg(126, 1))
+
+    print('\n\n')
+    sleep(100)
